5ade0ed7bf736739c29156891d55140de2659d36
Bardia <Bardiaa830@gmail.com>
2024-01-30 13:39:42 +0330

    Set up contacts route

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
-const express = require("express");
-const dotenv = require("dotenv").config()
+const express = require('express');
+const dotenv = require('dotenv').config()
 
 const app = express();
 
 const PORT = process.env.PORT || 6000;
 
-app.get('', (req, res) => {
-    res.send("ddkdkd");
-})
+app.use('/contacts', require('./routes/contactRoutes'))
 
 app.listen(PORT, () => {
-    console.log("H this bardia");
+    console.log("H this hshs");
 })
